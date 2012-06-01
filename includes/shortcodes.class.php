00ad9cef061a77be3c31815f78312fa7d5b423ba
flowcom_andreas <flowcom_andreas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-01 20:09:48 +0000

    Initial 1.1
    git-svn-id: https://plugins.svn.wordpress.org/flowsplit/trunk@551972 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/shortcodes.class.php
@@ -0,0 +1,68 @@
+<?php
+
+$flowsplit_shortcodes = new FlowSplit_ShortCodes();
+
+/**
+ * ShortCodes initialized
+ */
+class FlowSplit_ShortCodes{
+
+    function __construct(){
+
+        add_action('init', array(&$this, 'add_shortcodes'));
+
+    }
+
+    function add_shortcodes(){
+
+        add_filter('widget_text', 'do_shortcode');
+        add_shortcode( 'flowsplit', array(&$this, 'flowsplit') );
+
+    }
+
+    function flowsplit( $atts, $content = null ) {
+
+        $result = $content;
+
+    	extract( shortcode_atts( array(
+    		'id' => '0',
+            'option' => '0'
+    	), $atts ) );
+
+        if (!$atts['id']){
+
+            $result = "FlowSplit Error: flowsplit parameter 'id' is missing!";
+            return $result;
+
+        }
+
+        if (!$atts['option']){
+
+            $result = "FlowSplit Error: flowsplit parameter 'option' is missing!";
+            return $result;
+        }
+
+        $change = false;
+        $splits = get_transient( 'flowsplit' );
+        if (!is_array($splits)) {
+            $splits = array();
+            $change=true;
+        }
+        if (!in_array($atts['id'],$splits)) {
+            $splits[] = $atts['id'];
+            $change=true;
+        }
+        if ($change) set_transient( 'flowsplit', $splits );
+
+        $result = '<div data-flowsplit_id="'. $atts['id'] .'" data-flowsplit_option="'. $atts['option'] .'" class="flowsplit_content">' . $content . '</div>';
+
+        $result = str_replace('flowsplitreward', 'return flowsplit_reward(\''. $atts['id'] . '\',\''.$atts['option'].'\');', $result);
+
+    	return $result;
+    }
+
+
+}
+
+
+?>
---
+++ includes/tools.class.php
@@ -0,0 +1,181 @@
+<?php
+
+$flowsplit_tools = new FlowSplit_Tools();
+
+class FlowSplit_Tools{
+
+
+    function __construct() {
+
+        add_action('admin_menu', array(&$this, 'admin_menu'));
+        add_action( 'admin_enqueue_scripts', array(&$this, 'admin_enqueue_scripts') );
+        add_action('admin_head', array(&$this, 'admin_head') );
+
+    }
+
+    function admin_enqueue_scripts($hook){
+
+        if( 'tools_page_flowsplit_tools' != $hook )
+            return;
+        wp_enqueue_script( 'jsapi', 'https://www.google.com/jsapi' );
+        wp_enqueue_script( 'flowsplit_admin_tools', WP_PLUGIN_URL . '/flowsplit/js/admin_tools.js' );
+
+    }
+
+    function admin_menu(){
+
+        add_management_page( 'flowsplit', 'FlowSplit', 'read', 'flowsplit_tools', array(&$this, 'display')  );
+
+    }
+
+    function admin_head(){
+
+        echo "<script type=\"text/javascript\">
+            // Load the Visualization API and the piechart package.
+            google.load(\"visualization\", \"1\", {packages:[\"corechart\"]});";
+
+        $splits = get_transient('flowsplit');
+        foreach($splits as $key => $split){
+
+            $storage = get_transient( 'flowsplit_' . $split );
+
+            echo "google.setOnLoadCallback(flowsplit_".$split."_presentations_chart);
+
+                function flowsplit_".$split."_presentations_chart(){
+                    var data = google.visualization.arrayToDataTable([
+                      ['Options', 'Presentations'],";
+
+            foreach($storage as $s){
+
+                echo "['" . $s['value'] . "', " . $s['trials'] . "],";
+
+            }
+
+            echo "]);
+
+                    var options = {
+                      title: 'Presentations',
+                        backgroundColor: '#F6F6F6'
+                    };
+
+                    var chart = new google.visualization.PieChart(document.getElementById('flowsplit_" . $split . "_chart_presentations'));
+                    chart.draw(data, options);
+                }
+            ";
+
+            echo "google.setOnLoadCallback(flowsplit_".$split."_rewards_chart);
+
+                function flowsplit_".$split."_rewards_chart(){
+                    var data = google.visualization.arrayToDataTable([
+                      ['Options', 'Rewards'],";
+
+            foreach($storage as $s){
+
+                echo "['" . $s['value'] . "', " . $s['rewards'] . "],";
+
+            }
+
+            echo "]);
+
+                    var options = {
+                      title: 'Rewards',
+                        backgroundColor: '#F6F6F6'
+                    };
+
+                    var chart = new google.visualization.PieChart(document.getElementById('flowsplit_" . $split . "_chart_rewards'));
+                    chart.draw(data, options);
+                }
+            ";
+
+
+        }
+
+        echo "</script>";
+
+    }
+
+    function display(){
+
+        global $current_user;
+        get_currentuserinfo();
+
+        $plugin_data = get_plugin_data(WP_PLUGIN_DIR . '/flowsplit/flowsplit.php');
+        $version = $plugin_data['Version'];
+
+        ?>
+                <div class="wrap">
+                    <div id="icon-tools" class="icon32"><br></div>
+                    <h2><?php _e('FlowSplit Tools', 'flowsplit'); ?>, version <?php echo $version; ?></h2>
+
+                    <?php
+
+                    if (isset($_GET['delete'])){
+
+                        $id = esc_attr($_GET['delete']);
+                        $option = esc_attr($_GET['option']);
+
+                        if ($option && $id){
+
+                            $storage = get_transient( 'flowsplit_' . $id );
+                            unset($storage[$option]);
+                            set_transient('flowsplit_'.$id,$storage);
+
+                        }
+                        else if ($id){
+
+                            delete_transient( 'flowsplit_' . $id );
+                            $splits = get_transient( 'flowsplit' );
+                            foreach($splits as $key=>$split){
+                                if ($split == $id) unset($splits[$key]);
+                            }
+                            set_transient('flowsplit',$splits);
+
+                        }
+
+                    }
+
+                    ?>
+
+                    &nbsp;
+
+                    <?php
+                    $splits = get_transient('flowsplit');
+                    error_log(print_r($splits,true));
+                    foreach($splits as $key => $split){
+                        ?>
+                        <div class="postbox">
+                        <h3 style="padding:10px;"><?php echo $split; ?></h3>
+                        <div class="inside">
+                            <div id="flowsplit_<?php echo $split; ?>_chart_presentations" style="width: 400px; height: 300px; float:left;"></div>
+                            <div id="flowsplit_<?php echo $split; ?>_chart_rewards" style="width: 400px; height: 300px; float:left;"></div>
+                            <div style="clear:both;"></div>
+
+                            Clear all stats for <a href="?page=flowsplit_tools&delete=<?php echo $split; ?>" onclick="return confirm('Please, confirm delete stats for <?php echo $split; ?>');">[<?php echo $split; ?>]</a><br/>
+                            Clear stats for tag:
+                            <?php
+                            $storage = get_transient( 'flowsplit_' . $split );
+                            foreach($storage as $s){
+                                ?>
+                                <a href="?page=flowsplit_tools&delete=<?php echo $split; ?>&option=<?php echo $s['value']; ?>" onclick="return confirm('Please, confirm delete stats for <?php echo $s['value']; ?>');">[<?php echo $s['value'] ?>] </a>
+                                <?php
+                            }
+                            ?>
+
+                        </div>
+                        </div>
+                        <?php
+                    }
+                    ?>
+
+
+
+
+                </div>
+
+                <?php
+
+    }
+
+}
+
+?>
